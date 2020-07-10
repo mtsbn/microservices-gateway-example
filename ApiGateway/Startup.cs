f50004f3134e2aa3abe7622be37cb0d9d3ea82fc
mtsbn <matheus.nascimento@seuvale.com>
2020-07-10 17:51:08 -0300

    Add autheticatiom

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -6,6 +7,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace ApiGateway
 {
@@ -21,11 +25,44 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //auth logic
+
+            string textKey = "this is the secret key to add some default jwt token, lets see how it works";
+
+            var key = Encoding.ASCII.GetBytes(textKey);
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(textKey));
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = "chandra",
+                ValidateAudience = true,
+                ValidAudience = "enduser",
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true,
+            };
+
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+              .AddJwtBearer("TestKey", x =>
+              {
+                  x.RequireHttpsMetadata = false;
+                  x.TokenValidationParameters = tokenValidationParameters;
+              });
+
             services.AddOcelot(configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void
+            Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
